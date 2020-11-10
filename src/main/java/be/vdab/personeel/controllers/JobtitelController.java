97b10fc3974d9401be0f15679949c06ec78300d6
dmtwood <dimitri.gevers@gmail.com>
2020-11-10 18:48:46 +0100

    JobtitelController & jobtitels.java working

---
+++ src/main/java/be/vdab/personeel/controllers/JobtitelController.java
@@ -0,0 +1,58 @@
+package be.vdab.personeel.controllers;
+
+import be.vdab.personeel.domain.Jobtitel;
+import be.vdab.personeel.services.JobtitelService;
+import be.vdab.personeel.services.WerknemerService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/jobtitels")
+class JobtitelController {
+
+    private static final String JOBTITEL = "jobtitel";
+    private static final String JOBTITELS = "jobtitels";
+    private static final String WERKNEMER = "werknemer";
+    private static final String WERKNEMERS = "werknemers";
+
+    private final JobtitelService jobtitelService;
+    private final WerknemerService werknemerService;
+
+    public JobtitelController(JobtitelService jobtitelService, WerknemerService werknemerService) {
+        this.jobtitelService = jobtitelService;
+        this.werknemerService = werknemerService;
+    }
+
+    @GetMapping
+    public ModelAndView jobtitel() {
+        ModelAndView mAV = new ModelAndView(JOBTITELS);
+        mAV.addObject( JOBTITELS, jobtitelService.findAll() );
+        return mAV;
+    }
+
+    @GetMapping("{jobtitel}")
+    public ModelAndView specJobtitel(@PathVariable Optional<Jobtitel> jobtitel) {
+        ModelAndView mAV = new ModelAndView(JOBTITELS);
+        mAV.addObject( JOBTITELS, jobtitelService.findAll() );
+
+        jobtitel.ifPresent(
+                jobtitel1 -> mAV.addObject(
+                        JOBTITEL,
+                        jobtitel1)
+        );
+        jobtitel.ifPresent(
+                jobtitel1 -> mAV.addObject(
+                        WERKNEMERS,
+                        mAV.addObject(WERKNEMERS,werknemerService.findByJobtitel(jobtitel1))
+                )
+        );
+
+
+        return mAV;
+    }
+}
